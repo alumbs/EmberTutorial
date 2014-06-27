73469ff5e89895d9da84ad1de6ea76b29a2d6ee8
Chibuzor Alumba <coolchibs@gmail.com>
2014-06-27 18:43:09 -0500

    Revert "Done with the tutorial."
    This reverts commit 4fb2b29ffb7c9c2e1dcc08918498267cc2554d0c.

--- js/app.js
+++ js/app.js
@@ -1,54 +1,11 @@
 App = Ember.Application.create();
 
-App.Router.map(function(){
-	this.resource('about');
-	this.resource('posts', function(){
-		this.resource('post', { path: ':post_id'});
-	});
+App.Router.map(function() {
+  // put your routes here
 });
 
-App.PostsRoute = Ember.Route.extend({
-	model: function(){
-		return posts;
-	}
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
 });
-
-App.PostRoute = Ember.Route.extend({
-	model: function(params){
-		return posts.findBy('id', params.post_id);
-	}
-});
-
-App.PostController = Ember.ObjectController.extend({
-	isEditing: false,
-
-	actions: {
-		edit: function(){
-			this.set('isEditing', true);
-		},
-
-		doneEditing: function() {
-			this.set('isEditing', false);
-		}
-	}
-});
-
-Ember.Handlebars.helper('format-date', function(date){
-	return moment(date).fromNow();
-});
-
-var posts = [{
-	id: '1',
-	title: "Rails is Good",
-	author: {name: "sam"},
-	date: new Date('12-27-2014'),
-	excerpt: "Lorem ipsums",
-	body: "Some template info to use"
-}, {
-	id: '2',
-	title: "Rails is bad",
-	author: {name: "sam"},
-	date: new Date('12-27-2014'),
-	excerpt: "Lorem ipsumssss",
-	body: "Some other template info to be used"
-}];
